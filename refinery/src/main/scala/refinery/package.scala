73d72937887484694e011b6ad1d02fed635b9365
Alexey Kotlyarov <a@koterpillar.com>
2020-12-02 22:34:04 +1100

    Basic implementation

--- refinery/src/main/scala/refinery/package.scala
+++ refinery/src/main/scala/refinery/package.scala
@@ -1,17 +1,50 @@
 import cats._
-import cats.data.{Validated => CatsValidated, _}
+import cats.data._
 import cats.syntax.all._
 
 package object refinery {
-  type Validated[A] = Nested[ValidatedF, Context, A]
+  type ValidatedC[A] = Nested[ValidatedF, Context, A]
 
   type ValidatedF[A] = ValidatedNec[String, A]
   type Context[A]    = (Chain[String], A)
 
-  implicit class ValidatedOps[A](value: Validated[A]) {
+  private[refinery] def mapContext[A](value: ValidatedC[A])(fn: Chain[String] => Chain[String]): ValidatedC[A] = value.value match {
+    case Validated.Valid((ctx, a)) => validatedC((fn(ctx), a).valid)
+    case Validated.Invalid(e) => invalid(e)
+  }
+
+  private[refinery] def prependContext[A](context: Chain[String], value: ValidatedC[A]): ValidatedC[A] = mapContext(value)(context ++ _)
+
+  private[refinery] def validatedC[A](value: ValidatedF[Context[A]]): ValidatedC[A] = Nested[ValidatedF, Context, A](value)
+
+  private[refinery] def invalid[A](value: NonEmptyChain[String]): ValidatedC[A] = validatedC(value.invalid)
+
+  implicit class ValidatedOps[A](value: ValidatedC[A]) {
     def toEither: Either[NonEmptyChain[String], A] = value.value match {
-      case CatsValidated.Valid(a) => a._2.asRight[NonEmptyChain[String]]
-      case CatsValidated.Invalid(e) => e.asLeft[A]
+      case Validated.Valid((_, a)) => a.asRight[NonEmptyChain[String]]
+      case Validated.Invalid(e) => e.asLeft[A]
     }
+
+    def context(context: String): ValidatedC[A] = mapContext(value)(_.append(context))
+
+    def and[B](fn: A => ValidatedC[B]): ValidatedC[B] = value.value match {
+      case Validated.Valid((ctx, a)) => prependContext(ctx, fn(a))
+      case Validated.Invalid(e) => invalid(e)
+    }
+  }
+
+  implicit class ValueOps[A](value: A) {
+    def validC: ValidatedC[A] = value.pure[ValidatedC]
+  }
+
+  implicit class EitherOps[A](value: Either[String, A]) {
+    def toValidatedC: ValidatedC[A] = value match {
+      case Left(error) => error.invalidC
+      case Right(value) => value.validC
+    }
+  }
+
+  implicit class FailureOps(value: String) {
+    def invalidC[A]: ValidatedC[A] = invalid(NonEmptyChain(value))
   }
 }
--- refinery/src/test/scala/refinery/BasicTest.scala
+++ refinery/src/test/scala/refinery/BasicTest.scala
@@ -1,6 +1,7 @@
 package refinery
 
-import cats.data.{Validated => CatsValidated, _}
+import cats.data._
+import cats.syntax.all._
 
 class BasicTest extends munit.FunSuite {
   case class Connection(endpoint: String, port: Int)
@@ -10,19 +11,51 @@ class BasicTest extends munit.FunSuite {
                     followers: Vector[Connection],
                     )
 
-  def parseConfig(data: Map[String, String]): Validated[Cluster] = throw new Exception("Not implemented")
+  type Config = Map[String, String]
+
+  implicit class ConfigOps(value: Config) {
+    def project[A](prefix: String): ValidatedC[Config] = value.flatMap {
+      case (k, v) if k.startsWith(prefix) => Some(k.substring(prefix.length).dropWhile(_ == '.') -> v)
+      case _ => None
+    }.validC.context(prefix)
+
+    def only: ValidatedC[String] = value.get("") match {
+      case Some(result) => result.validC
+      case None => "No value found".invalidC
+    }
+  }
+
+  type Parser[A] = Config => ValidatedC[A]
+
+  def parseInt(value: String): ValidatedC[Int] = value.toIntOption match {
+    case Some(value) => value.validC
+    case None => s"Expected a number, found: '$value'".invalidC
+  }
+
+  def parseConnection(data: Config): ValidatedC[Connection] = (
+    data.project("endpoint").and(_.only),
+    data.project("port").and(_.only).and(parseInt),
+  ).mapN(Connection.apply)
+
+  def manyOf[A](parser: Parser[A]): Parser[Vector[A]] = config =>
+    LazyList.from(0).take(10).map(i => config.project(i.toString)).takeWhile(_.exists(_.nonEmpty)).toVector.sequence.and(_.traverse(parser))
+
+  def parseConfig(data: Config): ValidatedC[Cluster] = (
+    data.project("leader").and(parseConnection),
+    data.project("followers").and(manyOf(parseConnection)),
+  ).mapN(Cluster.apply)
 
   test("happy path") {
     val configData = Map(
       "leader.endpoint" -> "machine1",
       "leader.port" -> "1234",
-      "follower0.endpoint" -> "machine2",
-      "follower0.port" -> "5678",
-      "follower1.endpoint" -> "machine3",
-      "follower1.port" -> "5678",
+      "followers.0.endpoint" -> "machine2",
+      "followers.0.port" -> "5678",
+      "followers.1.endpoint" -> "machine3",
+      "followers.1.port" -> "5678",
     )
 
-    val result: Validated[Cluster] = parseConfig(configData)
+    val result: ValidatedC[Cluster] = parseConfig(configData)
 
     assertEquals(result.toEither, Right(
       Cluster(
@@ -39,12 +72,12 @@ class BasicTest extends munit.FunSuite {
     val badData = Map(
       "leader.endpoint" -> "machine1",
       "leader.port" -> "bad",
-      "follower0.endpoint" -> "machine2",
-      "follower0.port" -> "bad",
-      "follower1.port" -> "2345",
+      "followers.0.endpoint" -> "machine2",
+      "followers.0.port" -> "bad",
+      "followers.1.port" -> "2345",
     )
 
-    val result: Validated[Cluster] = parseConfig(badData)
+    val result: ValidatedC[Cluster] = parseConfig(badData)
 
     assertEquals(result.toEither, Left(NonEmptyChain(
       "leader: port: expected a number, found: 'bad'",
