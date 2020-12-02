7eb8375d14992da3f518cea82846ac9c7145a1f4
Alexey Kotlyarov <a@koterpillar.com>
2020-12-02 15:27:25 +1100

    Add a basic test

--- refinery/src/main/scala/refinery/package.scala
+++ refinery/src/main/scala/refinery/package.scala
@@ -1,10 +1,17 @@
 import cats._
-import cats.data._
+import cats.data.{Validated => CatsValidated, _}
 import cats.syntax.all._
 
 package object refinery {
   type Validated[A] = Nested[ValidatedF, Context, A]
 
-  type ValidatedF[A] = ValidatedNel[String, A]
+  type ValidatedF[A] = ValidatedNec[String, A]
   type Context[A]    = (Chain[String], A)
+
+  implicit class ValidatedOps[A](value: Validated[A]) {
+    def toEither: Either[NonEmptyChain[String], A] = value.value match {
+      case CatsValidated.Valid(a) => a._2.asRight[NonEmptyChain[String]]
+      case CatsValidated.Invalid(e) => e.asLeft[A]
+    }
+  }
 }
--- refinery/src/test/scala/refinery/BasicTest.scala
+++ refinery/src/test/scala/refinery/BasicTest.scala
@@ -1,9 +1,55 @@
 package refinery
 
+import cats.data.{Validated => CatsValidated, _}
+
 class BasicTest extends munit.FunSuite {
-  test("hello") {
-    val obtained = 42
-    val expected = 43
-    assertEquals(obtained, expected)
+  case class Connection(endpoint: String, port: Int)
+
+  case class Cluster(
+                    leader: Connection,
+                    followers: Vector[Connection],
+                    )
+
+  def parseConfig(data: Map[String, String]): Validated[Cluster] = throw new Exception("Not implemented")
+
+  test("happy path") {
+    val configData = Map(
+      "leader.endpoint" -> "machine1",
+      "leader.port" -> "1234",
+      "follower0.endpoint" -> "machine2",
+      "follower0.port" -> "5678",
+      "follower1.endpoint" -> "machine3",
+      "follower1.port" -> "5678",
+    )
+
+    val result: Validated[Cluster] = parseConfig(configData)
+
+    assertEquals(result.toEither, Right(
+      Cluster(
+        leader = Connection("machine1", 1234),
+        followers = Vector(
+          Connection("machine2", 5678),
+          Connection("machine3", 5678),
+        )
+      )
+    ))
+  }
+
+  test("multiple failures") {
+    val badData = Map(
+      "leader.endpoint" -> "machine1",
+      "leader.port" -> "bad",
+      "follower0.endpoint" -> "machine2",
+      "follower0.port" -> "bad",
+      "follower1.port" -> "2345",
+    )
+
+    val result: Validated[Cluster] = parseConfig(badData)
+
+    assertEquals(result.toEither, Left(NonEmptyChain(
+      "leader: port: expected a number, found: 'bad'",
+      "followers: 0: port: expected a number, found: 'bad'",
+      "followers: 1: endpoint: missing"
+    )))
   }
 }
